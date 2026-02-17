813a72b3059053e421f9d5b02a25891fa2da2e6c
KolyaPysarenko <90644330+kolyapisarenko@users.noreply.github.com>
2026-02-17 17:01:55 +0200

    IL-12 chore: remove database folder

--- src/database/__init__.py
+++
@@ -1,8 +0,0 @@
-from .connection import create_connection_pool, close_connection_pool
-from .queries import get_table
-
-__all__ = [
-    "create_connection_pool",
-    "close_connection_pool",
-    "get_table"
-]
